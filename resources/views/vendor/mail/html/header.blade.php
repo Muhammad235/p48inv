9691e523e19777b4a2839121b978cc915674b674
Muhammad235 <adelekeyahaya05@gmail.com>
2024-04-12 18:16:42 -1200

    Edit: mail logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 {{-- @if (trim($slot) === 'Laravel') --}}
-<img src="{{ asset('logo.png') }}" class="logo" style="width: 100%" alt="{{ config('app.name') }} Logo">
+<img src="{{ asset('logo.png') }}" class="logo"  alt="{{ config('app.name') }} Logo">
 {{-- @else --}}
 {{ $slot }}
 {{-- @endif --}}
